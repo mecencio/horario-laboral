45aef0a0dddcf6e65a5cfac589ea7e3c9d93f545
Nicolas Temprano <nicomxd22@gmail.com>
2025-06-19 23:01:43 -0300

    basic layout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,32 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterModule, RouterOutlet } from '@angular/router';
+import { Tooltip } from 'primeng/tooltip';
+import { ButtonModule } from 'primeng/button';
+import { SelectButton } from 'primeng/selectbutton';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    RouterModule,
+    Tooltip,
+    ButtonModule,
+    SelectButton,
+    FormsModule,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'horario-laboral';
+  currentYear: number = new Date().getFullYear();
+  routes: any[] = [];
+  workdayOptions: any[] = [
+    { name: '7.5 horas', value: 7.5 },
+    { name: '8 horas', value: 8 }
+  ];
+  selectedRoute: any = {};
+  selectedWorkdayOptions: any = { name: '7.5 horas', value: 7.5 };
+
 }
