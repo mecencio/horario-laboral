50295bbb359a040adbfa86b4ca6608f4e38fc7b3
Nicolas Temprano <nicomxd22@gmail.com>
2025-06-22 21:03:24 -0300

    Add DayTrackerComponent with routing and initial template

---
+++ src/app/features/day-tracker/day-tracker.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-day-tracker',
+  imports: [],
+  templateUrl: './day-tracker.component.html',
+  styleUrl: './day-tracker.component.scss'
+})
+export class DayTrackerComponent {
+
+}
