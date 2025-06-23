e4ba4990ffd4ddd724b6f6cc5c9dcbdb8c783484
Nicolas Temprano <nicomxd22@gmail.com>
2025-06-22 21:30:54 -0300

    Implement clock-in and clock-out functionality in DayTrackerComponent with updated template

--- src/app/features/day-tracker/day-tracker.component.ts
+++ src/app/features/day-tracker/day-tracker.component.ts
@@ -1,11 +1,25 @@
 import { Component } from '@angular/core';
+import { ButtonModule } from 'primeng/button';
 
 @Component({
   selector: 'app-day-tracker',
-  imports: [],
+  imports: [
+    ButtonModule,
+  ],
   templateUrl: './day-tracker.component.html',
   styleUrl: './day-tracker.component.scss'
 })
 export class DayTrackerComponent {
+  clockInTime: Date | null = null;
+  clockOutTime: Date | null = null;
 
+  clockIn() {
+    this.clockInTime = new Date();
+    console.log('Clock-in registered ' + this.clockInTime.toLocaleTimeString());
+  }
+
+  clockOut() {
+    this.clockOutTime = new Date();
+    console.log('Clock-out registered ' + this.clockOutTime.toLocaleTimeString());
+  }
 }
